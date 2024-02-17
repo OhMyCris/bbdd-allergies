4aa34fd8aa093d48cb4ea5c3e9d48787e19867f7
David <138037766+dhx101@users.noreply.github.com>
2024-02-17 21:13:11 +0100

    made some changes to schemas

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -1,29 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const salt = 10; // complejidad del encriptado
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-    {
-        name:{type: String, required: true, trim: true},
-        email:{type: String, unique: true, required: true, trim: true},
-        password:{type: String, required: true, trim: true},
-        allergies:{type: Object, required: false, trim: true},
-        favorites:[{type:Schema.Types.ObjectId, ref:"Food"}]
-    }, {
-        timestamps:true //te genera la fecha de creacion y modificacion del objeto
-    }
-)
+	{
+		name: { type: String, required: true, trim: true },
+		email: { type: String, unique: true, required: true, trim: true },
+		password: { type: String, required: true, trim: true },
+		phone: { type: String, required: true, trim: true },
+		allergies: { type: Object, required: false, trim: true },
+		favorites: [{ type: Schema.Types.ObjectId, ref: "Food" }],
+        diary:[{type: Schema.Types.ObjectId, ref: "Diario"}]
+	},
+	{
+		timestamps: true, //te genera la fecha de creacion y modificacion del objeto
+	}
+);
 
 userSchema.pre("save", (next) => {
-    console.log(this);
-    if (this.password) {
-        this.password = bcrypt.hashSync(this.password, salt);
-    }
-    next();
+	console.log(this);
+	if (this.password) {
+		this.password = bcrypt.hashSync(this.password, salt);
+	}
+	next();
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
